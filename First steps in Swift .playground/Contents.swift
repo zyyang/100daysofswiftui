f875aebb60c6cc63b46cf4d889b347731cfd4f84
杨振宇 <zyyang@fuguang.cn>
2020-11-24 21:59:07 +0800

    initial commit

---
+++ First steps in Swift .playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+var str = "Hello, playground 😄"
+
+var anotherstr = "Goodbye"
+
+
+var age = 41
+var population = 8_000_000
+var compare = 8000000
+
+
+population == compare
+
+var multiple_line_str = """
+Another Bite in the Dust.
+又吃一口灰
+"""
+
+var another_multiple_line_str = """
+The Queens \
+皇后乐队
+"""
+
+var pi:Double = 3.14159265358979
+var compare_pi = 3.14
+
+var awesome =  pi == compare_pi
+
+var str_result = "The two compared above is \(awesome) equal."
+
+let taylor = "swift"
